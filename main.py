bb8ee37f3d98b12babf6aecc2b2fb0f0c9d35d04
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2026-02-22 19:38:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ with tab1:
             
             </style>
             </head>
-            <body style="margin-top:10%">
+            <body style="margin-top:7.5%">
 
              
                         
